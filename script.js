f93900da05763b0cc759317c4e9e95d39d729838
MagiTom <mag.tomala1@gmail.com>
2021-03-03 20:07:58 +0100

    Draw shapes

---
+++ script.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.globalCompositeOperation = 'destination-over';
+
+let number = 0;
+let scale = 10;
+hue = Math.random() * 360;
+
+
+function drawFlower() {
+    let angle = number * 1;
+
+    let radius = scale * Math.sqrt(number);
+    let positionX = radius * Math.sin(angle) + canvas.width / 2;
+    let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+    ctx.strokeStyle = 'blue'
+    ctx.lineWidth = 5;
+    ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+    ctx.beginPath();
+    ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+    number++;
+    hue++;
+}
+
+function animate() {
+
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    drawFlower();
+    if (number > 200) return;
+    requestAnimationFrame(animate);
+
+
+}
+
+animate();
